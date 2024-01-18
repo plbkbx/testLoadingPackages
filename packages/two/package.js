a7aa99eccaa0ae2729fc4ff8e7a4ad1381261a8f
plb666 <pbeato@kubix.it>
2024-01-18 09:30:33 +0100

    Testing loading packages

---
+++ packages/two/package.js
@@ -0,0 +1,17 @@
+Package.describe({
+  name: 'two',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: '',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom(['2.14','3.0-beta.0']);
+  api.use('mongo');
+  api.addFiles('two.js');
+});
